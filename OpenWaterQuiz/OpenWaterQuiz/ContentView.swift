c2ec319d132a5782ee611ff93022e8521f75f72e
Toru Nakamichi <56828940+toruko44@users.noreply.github.com>
2023-04-16 23:25:07 +0900

    Add files via upload
    next quiz

--- OpenWaterQuiz/OpenWaterQuiz/ContentView.swift
+++ OpenWaterQuiz/OpenWaterQuiz/ContentView.swift
@@ -18,7 +18,7 @@ struct ContentView: View {
                     .multilineTextAlignment(.center)
                     .font(.title)
                 Spacer()
-                NavigationLink(destination: QuizView(num:2)){
+                NavigationLink(destination: QuizView(num:0)){
                     HStack {
                         Image(systemName: "play.circle")
                         Text("START")
@@ -30,87 +30,6 @@ struct ContentView: View {
     }
 }
 
-//解答画面
-//不正解時sheetを出す
-//問題表示画面をViewにしたらいけるかも
-struct QuizView:View{
-    @State var choice : String?
-    @State var sheet : Bool = false
-    @State var num : Int
-    var body: some View{
-        ZStack{
-            if QuizData[num].correct == choice{
-                Image(systemName: "circle")
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                    .padding(.top,30)
-                    .frame(width: 250,height: 250)
-            }
-            VStack{
-                    Text("\(QuizData[num].quiestion)")
-                        .padding(.vertical)
-                    Button("\(QuizData[num].answer1)"){
-                        choice = QuizData[num].answer1
-                        if choice != QuizData[num].correct{
-                            sheet = true
-                        }
-                    }
-                    .padding(.vertical)
-                    .sheet(isPresented:$sheet){
-                        WrongView()
-                    }
-                    Button("\(QuizData[num].answer2)"){
-                        choice = QuizData[num].answer2
-                        if choice != QuizData[num].correct{
-                            sheet = true
-                        }
-                    }
-                    .padding(.vertical)
-                    .sheet(isPresented:$sheet){
-                        WrongView()
-                    }
-                    Button("\(QuizData[num].answer3)"){
-                        choice = QuizData[num].answer3
-                        if choice != QuizData[num].correct{
-                            sheet = true
-                        }
-                    }
-                    .padding(.vertical)
-                    .sheet(isPresented:$sheet){
-                        WrongView()
-                    }
-                    Button("\(QuizData[num].answer4)"){
-                        choice = QuizData[num].answer4
-                        if choice != QuizData[num].correct{
-                            sheet = true
-                        }
-                    }
-                    .padding(.vertical)
-                    .sheet(isPresented:$sheet){
-                        WrongView()
-                    }
-                }
-        }
-    }
-}
-//不正解時に下から解説を出す
-struct WrongView:View{
-    var body: some View{
-        VStack{
-            VStack{
-                Image(systemName: "multiply")
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                    .padding(.top,30)
-                    .frame(width: 250,height: 250)
-                Spacer()
-                Text("\(QuizData[0].commentary)")
-                    .font(.title3)
-                Spacer()
-            }
-        }
-    }
-}
 
 
 struct ContentView_Previews: PreviewProvider {
@@ -122,5 +41,10 @@ struct ContentView_Previews: PreviewProvider {
 struct QuizView_Previews: PreviewProvider {
     static var previews: some View {
         QuizView(num:0)
+    }
+}
+struct Wrong_Previews: PreviewProvider {
+    static var previews: some View {
+        WrongView()
     }
 }
--- OpenWaterQuiz/OpenWaterQuiz/quiz_func.swift
+++ OpenWaterQuiz/OpenWaterQuiz/quiz_func.swift
@@ -5,6 +5,91 @@
 //  Created by 中道徹 on 2023/04/08.
 //
 
-import Foundation
+import SwiftUI
+//解答画面
+//不正解時sheetを出す
+//問題表示画面をViewにしたらいけるかも
+struct QuizView:View{
+    @State var choice : String?
+    @State var sheet : Bool = false
+    @State var num : Int
+    var body: some View{
+        ZStack{
+            if QuizData[num].correct == choice{
+                Image(systemName: "circle")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .padding(.top,30)
+                    .frame(width: 250,height: 250)
+            }
+            VStack{
+                    Text("\(QuizData[num].quiestion)")
+                        .padding(.vertical)
+                    Button("\(QuizData[num].answer1)"){
+                        choice = QuizData[num].answer1
+                        if choice != QuizData[num].correct{
+                            sheet = true
+                        }
+                    }
+                    .padding(.vertical)
+                    .sheet(isPresented:$sheet){
+                        WrongView()
+                    }
+                    Button("\(QuizData[num].answer2)"){
+                        choice = QuizData[num].answer2
+                        if choice != QuizData[num].correct{
+                            sheet = true
+                        }
+                    }
+                    .padding(.vertical)
+                    .sheet(isPresented:$sheet){
+                        WrongView()
+                    }
+                    Button("\(QuizData[num].answer3)"){
+                        choice = QuizData[num].answer3
+                        if choice != QuizData[num].correct{
+                            sheet = true
+                        }
+                    }
+                    .padding(.vertical)
+                    .sheet(isPresented:$sheet){
+                        WrongView()
+                    }
+                    Button("\(QuizData[num].answer4)"){
+                        choice = QuizData[num].answer4
+                        if choice != QuizData[num].correct{
+                            sheet = true
+                        }
+                    }
+                    .padding(.vertical)
+                    .sheet(isPresented:$sheet){
+                        WrongView()
+                    }
+                }
+        }
+    }
+}
 
-func preview_quiz(){}
+//不正解時に下から解説を出す
+struct WrongView:View{
+    var body: some View{
+        VStack{
+            VStack{
+                Image(systemName: "multiply")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .padding(.top,30)
+                    .frame(width: 250,height: 250)
+                Spacer()
+                Text("\(QuizData[0].commentary)")
+                    .font(.title3)
+                Spacer()
+            }
+        }
+    }
+}
+
+//func delay(number:Int) -> Int{
+//    var nextnumber = number + 1
+//    return nextnumber
+//}
