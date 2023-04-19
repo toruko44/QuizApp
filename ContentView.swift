af48400c3ceaed83a7f59bdc913e87a43d21acb1
Toru Nakamichi <56828940+toruko44@users.noreply.github.com>
2023-04-19 19:01:22 +0900

    Add files via upload
    問題の遷移を追加

---
+++ ContentView.swift
@@ -0,0 +1,41 @@
+//
+//  ContentView.swift
+//  OpenWaterQuiz
+//
+//  Created by 中道徹 on 2023/04/07.
+//
+
+import SwiftUI
+
+//最初の画面
+struct ContentView: View {
+    //let QuizData = QuizDataOriginal.shuffled()
+    var body: some View{
+        NavigationView{
+            VStack{
+                Spacer()
+                Text("オープンウォータライセンス\nクイズ")
+                    .multilineTextAlignment(.center)
+                    .font(.title)
+                Spacer()
+                NavigationLink(destination: flowView()){
+                    HStack {
+                        Image(systemName: "play.circle")
+                        Text("START")
+                    }.font(.title)
+                }
+                Spacer()
+            }
+        }
+    }
+}
+
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+
